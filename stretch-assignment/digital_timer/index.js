35c42385675254ae3e34a84a32bcf07ddd159d66
Landry Irakoze <irakozel@gmail.com>
2019-06-17 18:31:31 -0400

    timer font color fix

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -40,7 +40,7 @@ const toggle = () => {
 }
 
 const resetTimer = () => {
-    if (count === 1000) {
+    if (count === 1000 || count > 1000) {
         digits.forEach(item => item.classList.toggle('redDigit'));
     }
 
@@ -51,3 +51,65 @@ const resetTimer = () => {
     secondTens.textContent = 0;
 }
 
+// class Timer {
+
+//     timer;
+//     count = 0;
+
+//     constructor(timerElement) {
+//         this.timerElement = timerElement;
+
+//         this.secondTens = this.timerElement.querySelector('#secondTens');
+//         this.secondOnes = this.timerElement.querySelector('#secondOnes');
+//         this.msHundreds = this.timerElement.querySelector('#msHundreds');
+//         this.msTens = this.timerElement.querySelector('#msTens');
+//         this.digits = this.timerElement.querySelectorAll('.digits .digit');
+
+//         this.startBtn = this.timerElement.querySelector('#start');
+//         this.resetBtn = this.timerElement.querySelector('#reset');
+
+//         this.startBtn.addEventListener('click', () => this.toggle());
+//         this.resetBtn.addEventListener('click', () => this.resetTimer());
+
+//     }
+
+//     toggle() {
+//         if (this.startBtn.textContent === 'Start') {
+//             this.startBtn.textContent = 'Stop';
+//             this.startBtn.classList.toggle('stop');
+//             this.timer = setInterval(this.increment(), 10);
+//         } else if (this.startBtn.textContent === 'Stop') {
+//             this.startBtn.textContent = 'Start';
+//             this.startBtn.classList.toggle('stop');
+//             clearInterval(this.timer);
+//         }
+//     }
+
+//     resetTimer() {
+//         if (this.count === 1000) {
+//             this.digits.forEach(item => item.classList.toggle('redDigit'));
+//         }
+    
+//         this.count = 0;
+//         msTens.textContent = 0
+//         msHundreds.textContent = 0;
+//         secondOnes.textContent = 0;
+//         secondTens.textContent = 0;
+//     }
+
+//     increment() {
+//         this.count++;
+//         msTens.textContent = this.count % 10;
+//         msHundreds.textContent = Math.floor(this.count / 10) % 10;
+//         secondOnes.textContent = Math.floor(this.count / 100) % 10;
+//         secondTens.textContent = Math.floor(this.count / 1000) % 10;
+//         if (this.count === 1000) {
+//             clearInterval(this.timer);
+//             this.digits.forEach(item => item.classList.toggle('redDigit'))
+//         }
+//     }
+// }
+
+// const timerContainer = document.querySelector('.timer-container');
+
+// let myTimer = new Timer(timerContainer);
