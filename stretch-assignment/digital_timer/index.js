a2839f2001b81b5bdb15003587d893d26900c4d9
Landry Irakoze <irakozel@gmail.com>
2019-06-17 16:50:47 -0400

    timer functionality

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,47 @@
+//Buttons
+const startBtn = document.querySelector('#start');
+const resetBtn = document.querySelector('#reset');
+
+startBtn.addEventListener('click', () => toggle());
+resetBtn.addEventListener('click', () => resetTimer());
+
+//Digit Elements
+const secondTens = document.querySelector('#secondTens');
+const secondOnes = document.querySelector('#secondOnes');
+const msHundreds = document.querySelector('#msHundreds');
+const msTens = document.querySelector('#msTens');
+const digits = document.querySelectorAll('.digits .digit');
+
+let count = 0;
+let timer;
+
+const increment = () => {
+    count++;
+    msTens.textContent = count % 10;
+    msHundreds.textContent = Math.floor(count / 10) % 10;
+    secondOnes.textContent = Math.floor(count / 100) % 10;
+    secondTens.textContent = Math.floor(count / 1000) % 10;
+    if (count === 1000) {
+        clearInterval(timer);
+    }
+}
+
+const toggle = () => {
+    if (startBtn.textContent === 'Start') {
+        startBtn.textContent = 'Stop';
+        startBtn.classList.toggle('stop');
+        timer = setInterval(increment, 10);
+    } else if (startBtn.textContent === 'Stop') {
+        startBtn.textContent = 'Start';
+        startBtn.classList.toggle('stop');
+        clearInterval(timer);
+    }    
+}
+
+const resetTimer = () => {
+    count = 0;
+    msTens.textContent = 0
+    msHundreds.textContent = 0;
+    secondOnes.textContent = 0;
+    secondTens.textContent = 0;
+}
